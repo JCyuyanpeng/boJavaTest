d4d1bd2a6319a8060b2a3dbab091308e2a144c58
BBC\CB <3288874950@qq.com>
2022-04-01 22:04:28 +0800

    更新创建helloworld 方法

--- javaTest/src/com/javaTest/hello.java
+++ javaTest/src/com/javaTest/hello.java
@@ -4,4 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void helloWorld(){
+        System.out.println("new prod");
+    }
 }
