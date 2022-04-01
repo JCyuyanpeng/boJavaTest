5997fe9013d22394e515668d4387c9b034c3f9d0
BBC\CB <3288874950@qq.com>
2022-04-01 22:12:37 +0800

    new test

--- javaTest/src/com/javaTest/hello.java
+++ javaTest/src/com/javaTest/hello.java
@@ -1,10 +1,22 @@
 package com.javaTest;
 
+import java.util.Scanner;
+
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("please input int");
+        int new_number = number(10);
+        System.out.println(new_number);
     }
     public static void helloWorld(){
         System.out.println("new prod");
+    }
+    public static int number(int x){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (x > num){
+            return x;
+        }
+        return num;
     }
 }
