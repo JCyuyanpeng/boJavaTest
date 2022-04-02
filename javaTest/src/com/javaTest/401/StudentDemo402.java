68ffa5cae3bee1e1133471c6405d0dc5300da527
BBC\CB <3288874950@qq.com>
2022-04-02 14:03:57 +0800

    切换文件夹

---
+++ javaTest/src/com/javaTest/401/StudentDemo402.java
@@ -0,0 +1,12 @@
+package com.javaTest;
+
+public class StudentDemo402 {
+    public static void main(String[] args) {
+        Student402 s = new Student402();
+        s.setName("tom");
+        s.setAge(30);
+        s.show();
+        System.out.println("-------");
+        System.out.println(s.getName() + "..." + s.getAge());
+    }
+}
